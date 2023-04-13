47a816a2755354f7378249a3492f78dce38d3869
장준혁 <a01056169474@gmail.com>
2023-04-13 02:43:19 +0000

    bmi 수치 계산기

---
+++ bmi.c
@@ -0,0 +1,17 @@
+//made by junhyuk
+//bmi curicular
+//ansan technology highschool
+
+#include <stdio.h>
+int main()
+{
+	int num1 = 0;
+	int num2 = 0;
+	
+	printf("this program is an addition calculator\ninput the first number! : ");
+	scanf("%d", &num1);
+	printf("input the second numner! : ");	
+	scanf("%d", &num2);
+	printf("The result of the carculation is...(%d\n)", num1 * num2);
+	return 0;
+}
